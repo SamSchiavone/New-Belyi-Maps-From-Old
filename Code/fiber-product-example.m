65e7b1364eee5a142cdf8c919f9c1db11b3cc828
Sam Schiavone <sam.schiavone@gmail.com>
2022-12-17 02:50:53 -0500

    more fiber product examples

--- Code/fiber-product-example.m
+++ Code/fiber-product-example.m
@@ -1,17 +1,41 @@
+ChangeDirectory("~/github/BelyiDB");
+load "config.m";
 AttachSpec("~/github/Belyi/Code/spec");
 AttachSpec("~/github/Gm-Reduce/spec");
-LMFDBLabelToBelyiDBLabel("5T5-5_2.1.1.1_3.2-a");
+
+function FiberProduct(phi1, phi2)
+  // Form fiber product of phi1 and phi2
+  f1 := PlaneModel(phi1);
+  f2 := PlaneModel(phi2);
+  K1 := BaseRing(Parent(f1));
+  R<t,u,v> := PolynomialRing(K1,3);
+  f1 := Evaluate(f1, [t,u]);
+  f2 := Evaluate(f2, [t,v]);
+  return Curve(Spec(R), [f1,f2]);
+end function;
+
+function FiberProductPlane(phi1, phi2)
+  C := FiberProduct(phi1, phi2);
+  fs := DefiningEquations(C);
+  assert #fs eq 2;
+  R<t,u,v> := Parent(fs[1]);
+  K1 := BaseRing(R);
+  f3 := Resultant(fs[1], fs[2], t);
+  S<X,Y> := PolynomialRing(K1,2);
+  f3 := Evaluate(f3, [0, X, Y]);
+  return Curve(Spec(S), f3);
+end function;
+
+LMFDBLabelToBelyiDBLabel("5T5-5_2.1.1.1_3.2-a" : dot_m := true);
 lab1 := $1;
 s1 := BelyiDBRead(lab1);
-BelyiDBFilenames(5);
-K1<nu1> := RationalsAsNumberField();
-K1<nu1> := K1;
-X1 := Curve(ProjectiveSpace(PolynomialRing(K1, 2)));
-KX1<x> := FunctionField(X1);
-phi1 := KX1!(3/2*x^5/(x^5 - 5/2*x^4 - 5/4*x^3 + 45/8*x^2 - 27/8));
+phi1 := s1`BelyiDBBelyiMaps[1];
+X1 := s1`BelyiDBBelyiCurves[1];
+K1 := BaseField(X1);
+//X1 := Curve(ProjectiveSpace(PolynomialRing(K1, 2)));
+//KX1<x> := FunctionField(X1);
+//phi1 := KX1!(3/2*x^5/(x^5 - 5/2*x^4 - 5/4*x^3 + 45/8*x^2 - 27/8));
 phi1;
-lab1 *:= ".m";
-s1 := BelyiDBRead(lab1);
 LMFDBLabelToBelyiDBLabel("3T2-3_2.1_2.1-a" : dot_m := true);
 lab2 := $1;
 s2 := BelyiDBRead(lab2);
@@ -20,24 +44,41 @@ phi2 := $1;
 s`BelyiDBBelyiCurves[1];
 s2`BelyiDBBelyiCurves[1];
 X2 := $1;
-PlaneModel(phi1);
-f1 := $1;
-PlaneModel(phi2);
-f2 := $1;
-R<t,u,v> := PolynomialRing(K1,3);
-Evaluate(f1, [t,u]);
-f1 := $1;
-Evaluate(f2, [t,v]);
-f2 := $1;
-Resultant(f1, f2, t);
-f3 := $1;
-Parent(f3);
-S<X,Y> := PolynomialRing(K1,2);
-f3 := Evaluate(f3, [0, X, Y]);
-C3 := Curve(Spec(S), f3);
+
+C3 := FiberProduct(phi1,phi2);
 C3;
-Genus(C3);
-IsIrreducible(f3);
-D3 := Curve(Spec(R), [f1,f2]);
+D3 := FiberProductPlane(phi1, phi2);
 D3;
-Genus(D3);
+
+names := BelyiDBFilenames(5);
+for name in names do
+  s := BelyiDBRead(name);
+  phi := s`BelyiDBBelyiMaps[1];
+  C := FiberProduct(phi, phi2);
+  print C;
+  print Genus(C);
+end for;
+
+/*
+  PlaneModel(phi1);
+  f1 := $1;
+  PlaneModel(phi2);
+  f2 := $1;
+  R<t,u,v> := PolynomialRing(K1,3);
+  Evaluate(f1, [t,u]);
+  f1 := $1;
+  Evaluate(f2, [t,v]);
+  f2 := $1;
+  Resultant(f1, f2, t);
+  f3 := $1;
+  Parent(f3);
+  S<X,Y> := PolynomialRing(K1,2);
+  f3 := Evaluate(f3, [0, X, Y]);
+  C3 := Curve(Spec(S), f3);
+  C3;
+  Genus(C3);
+  IsIrreducible(f3);
+  D3 := Curve(Spec(R), [f1,f2]);
+  D3;
+  Genus(D3);
+*/
